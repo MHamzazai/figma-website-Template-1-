50a09353171d11136bf4fd485c4f30512cb7d76e
MHamzazai <farhatifraqfarhatifraq@gmail.com>
2025-01-17 16:11:51 +0500

    reomove extra import

--- src/sanity/sanity.config.ts
+++ src/sanity/sanity.config.ts
@@ -4,16 +4,13 @@ dotenv.config(); // load the environment vaiables
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
-import schemas from "./schema/mainSchema";
 
 export const sanityConfig = defineConfig({
     name: "MarketPlace Builder",
     title: "A General E-commerce Website",
     plugins: [structureTool(), visionTool()],
     basePath: "/sanityStudio",
-    schema: {
-        types: schemas,
-    },
+    schema: {},
     projectId: process.env.SANITY_PROJECT_ID || "gz7a88fw",
     dataset: process.env.SANITY_DATASET || "production",
 }); 
